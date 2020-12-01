1451602f3abb729df254332e4fa8d16a8435a441
Arora-Yash-coder <yasharora9416@gmail.com>
2020-11-30 18:13:38 -0800

    Lab 6 Work

--- src/ca/bcit/comp2522/labs/lab06/Serial.java
+++ src/ca/bcit/comp2522/labs/lab06/Serial.java
@@ -5,6 +5,11 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 public class Serial {
     public static void main (String[] args) {
+        String levelName = "Lava";
+        int levelNumber = 10;
+        int numberOfEnemiers = 20;
+
+
         try {
             BufferedImage img = ImageIO.read(new File("lava.png"));
             Sprite s = new Sprite(img);
@@ -14,12 +19,19 @@ public class Serial {
             FileOutputStream f = new FileOutputStream("sprite.ser");
             ObjectOutput out = new ObjectOutputStream(f);
             out.writeObject(s);
+            out.writeChars(levelName);
+            out.writeInt(levelNumber);
+            out.writeInt(numberOfEnemiers);
             out.flush();
             out.close();
 
             FileInputStream f2 = new FileInputStream("sprite.ser");
             ObjectInputStream in = new ObjectInputStream(f2);
             Sprite s2 = (Sprite) in.readObject();
+            levelName = (String) in.readUTF();
+            levelNumber = (int) in.read();
+            numberOfEnemiers = (int) in.read();
+
             System.out.println(s2.getPixelData()[4]);
             System.out.println(s.getPixelData().length);
             in.close();
--- src/ca/bcit/comp2522/labs/lab06/Sprite.java
+++ src/ca/bcit/comp2522/labs/lab06/Sprite.java
@@ -13,6 +13,14 @@ public class Sprite implements Serializable {
     public ArrayList<String> weapons;
     public byte[] pixelData;
 
+    public Sprite(int x, int y, int health, float gravity, ArrayList<String> weapons) {
+        this.x = x;
+        this.y = y;
+        this.health = health;
+        this.gravity = gravity;
+        this.weapons = weapons;
+    }
+
     public Sprite(final BufferedImage img) {
         pixelData = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
     }
@@ -21,6 +29,47 @@ public class Sprite implements Serializable {
         return pixelData;
     }
 
+    public int getX() {
+        return x;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
 
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public float getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(float gravity) {
+        this.gravity = gravity;
+    }
+
+    public ArrayList<String> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(ArrayList<String> weapons) {
+        this.weapons = weapons;
+    }
+
+    public void setPixelData(byte[] pixelData) {
+        this.pixelData = pixelData;
+    }
 }
